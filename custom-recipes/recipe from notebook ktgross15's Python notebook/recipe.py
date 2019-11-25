7b0f9c1c3b78abfe7c331ac1edcd3f5e2965d1ad
Katie Gross <ktgross15@gmail.com>
2019-11-25 13:43:49 -0500

    SSL cred stuff

--- custom-recipes/recipe from notebook ktgross15's Python notebook/recipe.py
+++ custom-recipes/recipe from notebook ktgross15's Python notebook/recipe.py
@@ -6,31 +6,10 @@ import pandas as pd
 import datetime
 import json
 
-
-### READ INPUT PARAMETERS ###
-
-def read_parameter(num, param):
-    param_name = 'instance{}_{}'.format(num, param)
-    param_val = get_recipe_config().get(param_name, None)
-    # abc
-    return param_val
-
-user_limit_per_license = get_recipe_config().get('limit_per_license')
-num_instances = int(get_recipe_config().get('num_instances'))
-
-urls = []
-api_keys = []
-
-for num in range(1, num_instances+1):
-    # read parameter values
-    url = read_parameter(num, 'url')
-    api_key = read_parameter(num, 'api_key')
-
-    # append to lists
-    api_keys.append(api_key)
-    urls.append(url)
-
 ### GENERATE FULL DATAFRAME ###
+
+api_url_dict = get_recipe_config().get("urls_keys", None)
+ignore_ssl_certs = get_recipe_config().get("ignore_ssl_certs", None)
 
 # instantiate lists
 display_names = []
@@ -38,28 +17,34 @@ emails = []
 user_groups = []
 logins = []
 user_profs = []
+prof_limit_list = []
 
-now = datetime.datetime.now()
+full_df = pd.DataFrame(columns=['license_id','instance_url','display_name','login','email','user_profile','user_groups'])
 
-full_df = pd.DataFrame(columns=['instance_url','display_name','login','email','user_profile','user_groups'])
-
-for url, api_key in zip(urls, api_keys):
-    print url
+for url, api_key in api_url_dict.iteritems():
     client = dataikuapi.DSSClient(url, api_key)
+    if ignore_ssl_certs:
+        client._session.verify = False
 
-    # get license id
+    # get licensing status if client creds are valid
     licensing_status = client.get_licensing_status()
     license_id = licensing_status['base']['licenseContent']['licenseId']
+    
+    # add to prof limit dict
+    prof_limits = licensing_status['limits']['profileLimits']
+    for user_prof, vals in prof_limits.iteritems():
+        limit = prof_limits[user_prof]['licensed']['licensedLimit']
+        prof_limit_list.append([license_id, user_prof, limit])
 
     # get user-specific info
     for user in client.list_users():
 
-        display_name = user['displayName']
+        display_name = user['displayName'].lower()
         user_groups = user['groups']
-        login = user['login']
-        user_prof = user['userProfile']
+        login = user['login'].lower()
+        user_prof = user['userProfile'].lower()
         try:
-            email = user['email']
+            email = user['email'].lower()
         except:
             email = np.NaN
 
@@ -71,58 +56,38 @@ for url, api_key in zip(urls, api_keys):
                                   'user_profile':user_prof,
                                   'user_groups':user_groups},
                                 ignore_index=True)
+    print "Added ", url
+
+now = datetime.datetime.now()
+
+# generate instance-license dataframe
+instance_df = full_df.groupby(['login','user_profile','license_id','instance_url']).count().reset_index()
+instance_df = instance_df.drop(['display_name','email','user_groups'], axis=1)
+instance_df['time_recorded'] = now
 
 
-### GENERATE USER, INSTANCE, AND LICENSE LEVEL DATAFRAMES ###
+# generate overall dataframe
+counts_df = full_df[['login','license_id','user_profile']].groupby(['license_id','user_profile']).count().reset_index()
+counts_df = counts_df.rename(columns={'login':'count'})
 
-def generate_user_df(full_df, pivot_col, new_total_col):
-    pivoted_df = full_df[['login','license_id','instance_url']].pivot_table(index='login', columns=pivot_col, aggfunc='count')
+limits_df = pd.DataFrame(prof_limit_list).drop_duplicates()
+limits_df = limits_df.rename(columns={0:'license_id', 1:'user_profile', 2: 'limit'})
+limits_df['user_profile'] = limits_df['user_profile'].str.lower()
 
-    # reformat column names
-    pivoted_df.columns = pivoted_df.columns.droplevel(level=0)
-    pivoted_df.columns.name = None
-    pivoted_df = pivoted_df.reset_index()
-    
-    pivoted_df.fillna(0, inplace=True)
-    pivoted_df[new_total_col] = pivoted_df.sum(axis=1)
-
-    for col in pivoted_df.columns[1:]:
-        pivoted_df[col] = pivoted_df[col].astype(int)
-
-    return pivoted_df
-
-users_licenses_df = generate_user_df(full_df, 'license_id', 'total_licenses_for_user')
-users_instances_df = generate_user_df(full_df, 'instance_url', 'total_instances_for_user')
-
-# generate license dataframe
-license_df = full_df.groupby(['license_id'])['login'].nunique().to_frame().reset_index()
-license_df = license_df.rename(columns={'login':'distinct_users_LICENSE'})
-
-# generate instance dataframe
-instance_df = full_df.groupby(['instance_url','license_id'])['login'].nunique().to_frame().reset_index()
-instance_df = instance_df.rename(columns={'login':'distinct_users_INSTANCE'})
-
-# merge license and instance info
-merged_df = pd.merge(instance_df, license_df, on='license_id')
-cols = ['instance_url','distinct_users_INSTANCE','license_id','distinct_users_LICENSE']
-merged_df = merged_df[cols]
-
-# add license limit cols
-# check if it should be < OR <= !!! #######
-merged_df['user_limit_LICENSE'] = user_limit_per_license
-merged_df['within_limit'] = np.where(merged_df['distinct_users_LICENSE'] <= merged_df['user_limit_LICENSE'], True, False)
+overall_df = pd.merge(counts_df, limits_df, on=['license_id','user_profile'], how='right')
+overall_df = overall_df.fillna(0)
+overall_df['count'] = overall_df['count'].astype(int)
+overall_df = overall_df.sort_values(by=['license_id','user_profile'])
+overall_df['within_limit'] = np.where((overall_df['count']<= overall_df['limit']) | (overall_df['limit'] == -1), True, False)
+overall_df['time_recorded'] = now
 
 
 ### WRITE TO  OUTPUT DATASETS ###
 
-users_by_license_ds_name = get_output_names_for_role('users_by_license')[0]
+users_by_license_ds_name = get_output_names_for_role('user_licenses')[0]
 users_by_license = dataiku.Dataset(users_by_license_ds_name)
-users_by_license.write_with_schema(users_licenses_df)
-
-users_by_instance_ds_name = get_output_names_for_role('users_by_instance')[0]
-users_by_instance = dataiku.Dataset(users_by_instance_ds_name)
-users_by_instance.write_with_schema(users_instances_df)
+users_by_license.write_with_schema(instance_df)
 
 overall_metrics_ds_name = get_output_names_for_role('overall_metrics')[0]
 overall_metrics = dataiku.Dataset(overall_metrics_ds_name)
-overall_metrics.write_with_schema(merged_df)
+overall_metrics.write_with_schema(overall_df)
